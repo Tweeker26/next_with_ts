035918d193c9e0e44afec607e77f3a9648bd235e
tweeker26 <ivanwin26@gmail.com>
2020-11-25 10:40:13 +0300

    Fix viewport and styles for fonts

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>{siteTitle}</title>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
 
       <Component {...pageProps} />
