b00a4da6872a3f4cdae839aca01c746993ff08da
tweeker26 <ivanwin26@gmail.com>
2020-11-25 11:27:41 +0300

    some styles

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ module.exports = {
     UNSPLASH_ACCESS_KEY: process.env.UNSPLASH_ACCESS_KEY,
     UNSPLASH_USER: process.env.UNSPLASH_USER,
   },
+  images: {
+    domains: ['images.unsplash.com'],
+  },
 };
